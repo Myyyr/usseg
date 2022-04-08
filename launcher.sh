585eccd53d97013f633d1a2757f0d90b7d5fd80f
MYR <loic.themyr@gmail.com>
2022-04-08 16:40:39 +0200

    set up  - debug

--- launcher.sh
+++ launcher.sh
@@ -8,7 +8,8 @@
 
 
 source /opt/server-env.sh
-source /home/lthemyr/usenv/bin/activate
+# source /home/lthemyr/usenv/bin/activate
+conda activate usenv
 
-
-srun python main.py -m model=nnunet
+srun python main.py
+# srun python main.py -m model=nnunet
