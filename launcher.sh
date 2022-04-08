666159acd6df7e668aec0ff2ac1090a2caf8bf3a
MYR <loic.themyr@gmail.com>
2022-04-08 18:39:56 +0200

    set up  - debug

--- launcher.sh
+++ launcher.sh
@@ -3,13 +3,13 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/nnunet.out # output file name
-#SBATCH --error=logs/nnunet.err  # error file name
+#SBATCH --output=logs/fine.out # output file name
+#SBATCH --error=logs/fine.err  # error file name
 
 
 source /opt/server-env.sh
 # source /home/lthemyr/usenv/bin/activate
 conda activate usenv
 
-# srun python main.py
-srun python main.py -m model=nnunet training.dbg=False
+srun python main.py training.dbg=False
+# srun python main.py -m model=nnunet training.dbg=False
