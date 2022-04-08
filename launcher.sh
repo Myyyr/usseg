0cbb7a9cfb1bd8a8c4015f4be3fd4b17aa86e9a6
MYR <loic.themyr@gmail.com>
2022-04-08 19:15:04 +0200

    set up  - debug

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/fine.out # output file name
-#SBATCH --error=logs/fine.err  # error file name
+#SBATCH --output=logs/nnunet.out # output file name
+#SBATCH --error=logs/nnunet.err  # error file name
 
 
 source /opt/server-env.sh
