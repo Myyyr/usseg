28594af0f26ed142689d88137f5b313a28c0daff
MYR <loic.themyr@gmail.com>
2022-04-22 11:33:25 +0200

    set up  - debug

--- Trainer.py
+++ Trainer.py
@@ -217,6 +217,8 @@ class Trainer():
 				# if len(self.net_num_pool_op_kernel_sizes)==0:
 					# output = torch.softmax(output[0], dim=1)
 					# labels = labels[0]
+				log.debug('input', inputs.device)
+				log.debug('labels', labels.device)
 				l = self.loss(output, labels)
 				l.backward()
 				l_train += l.detach().cpu().numpy()
