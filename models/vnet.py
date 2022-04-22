68b8f916d21c94c6c93d2d2a2d8f888b11e6be8c
MYR <loic.themyr@gmail.com>
2022-04-22 09:41:34 +0200

    set up  - debug

--- models/vnet.py
+++ models/vnet.py
@@ -4,7 +4,7 @@ from nnunet.network_architecture.neural_network import SegmentationNetwork
 
 class model(SegmentationNetwork):
 	def __init__(self,spatial_dims=3, in_channels=1, num_classes=2, *args, **kwargs):
-		self.network = vnet(out_chanels=num_classes)
+		self.network = vnet.VNet(out_chanels=num_classes)
 
 	def forward(self, x, *args, **kwargs):
 		return self.network(x)
