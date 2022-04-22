216925f9685c8fd63676dcf2400ca0c32e403e3b
MYR <loic.themyr@gmail.com>
2022-04-22 11:25:20 +0200

    set up  - debug

--- launcher.sh
+++ launcher.sh
@@ -14,4 +14,6 @@ conda activate usenv
 # srun python main.py training.dbg=False
 # srun python main.py -m model=nnunet # training.dbg=False
 
-srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128
+# srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128
+srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V4 training.epochs=1400 training.batch_size=4
+# srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V5 training.epochs=2000 training.batch_size=4
