2d70657714a2da197170b24d8c5b30354501de2a
MYR <loic.themyr@gmail.com>
2022-04-22 11:27:14 +0200

    set up  - debug

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/nnunet_small_v4.out # output file name
-#SBATCH --error=logs/nnunet_small_v4.err  # error file name
+#SBATCH --output=logs/nnunet_small_v5.out # output file name
+#SBATCH --error=logs/nnunet_small_v5.err  # error file name
 
 
 source /opt/server-env.sh
@@ -15,5 +15,5 @@ conda activate usenv
 # srun python main.py -m model=nnunet # training.dbg=False
 
 # srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128
-srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V4 training.epochs=1400 training.batch_size=4
-# srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V5 training.epochs=2000 training.batch_size=4
+# srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V4 training.epochs=1400 training.batch_size=4
+srun python main.py -m model=nnunet dataset=us128 training=crop128_128_128 training.name=CROP_SMALL_V5 training.epochs=2000 training.batch_size=4
