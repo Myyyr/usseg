380f59507262d172b1b142214efca18df8d34cc6
MYR <loic.themyr@gmail.com>
2022-05-31 15:08:11 +0200

    cotr

--- Trainer.py
+++ Trainer.py
@@ -286,7 +286,8 @@ class Trainer():
 				# log.debug("labels", labels.shape)
 				l = self.loss(output, labels)
 				l.backward()
-				log.debug("l", l.item().shape)
+				log.debug("l", l.shape)
+				log.debug("l", l.item())
 				log.debug("l", l)
 				exit(0)
 
