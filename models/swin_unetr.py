a1b233b7932d916841286988077634aab4928651
MYR <loic.themyr@gmail.com>
2022-06-01 16:16:01 +0200

    swin_untr

--- models/swin_unetr.py
+++ models/swin_unetr.py
@@ -9,7 +9,7 @@ class model(SegmentationNetwork):
             out_channels=num_classes, 
             img_size=img_size, 
             feature_size=feature_size,
-            depths=(2, 2, 4, 2),
+            depths=(2, 2, 2, 2),
             norm_name='batch')
 
     def forward(self, x, *args, **kwargs):
