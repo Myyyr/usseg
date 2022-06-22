d92b55b925cafeacbfc23f123920964e9714b03d
MYR <loic.themyr@gmail.com>
2022-06-22 16:39:51 +0200

    glam ircad

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/glam_128on128.out # output file name
-#SBATCH --error=logs/glam_128on128.err  # error file name
+#SBATCH --output=logs/glam_128on256.out # output file name
+#SBATCH --error=logs/glam_128on256.err  # error file name
 
 
 source /opt/server-env.sh
@@ -26,7 +26,7 @@ conda activate usenv
 
 
 # Runs on 128x128x128 images
-srun python main.py -m model=glam dataset=us128 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
+# srun python main.py -m model=glam dataset=us128 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
 
 # Runs on 256x256x256 images
-# srun python main.py -m model=glam dataset=us256 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
+srun python main.py -m model=glam dataset=us256 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
