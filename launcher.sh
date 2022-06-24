413549de74b6e07d832771c64bfc665b3cf9e12b
MYR <loic.themyr@gmail.com>
2022-06-24 15:56:29 +0200

    glam ircad

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/nnunet_128on256.out # output file name
-#SBATCH --error=logs/nnunet_128on256.err  # error file name
+#SBATCH --output=logs/nnunet_64on256.out # output file name
+#SBATCH --error=logs/nnunet_64on256.err  # error file name
 
 
 source /opt/server-env.sh
@@ -25,9 +25,12 @@ conda activate usenv
 
 
 
-# Runs on 128x128x128 images
+# Runs 128x128x128 on 128x128x128 images
 # srun python main.py -m model=glam dataset=us128 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
 
-# Runs on 256x256x256 images
+# Runs 128x128x128 on 256x256x256 images
 # srun python main.py -m model=glam dataset=us256 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
-srun python main.py -m model=nnunet dataset=us256 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
+# srun python main.py -m model=nnunet dataset=us256 training=crop128_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
+
+# Runs 64x128x128 on 256x256x256 images
+srun python main.py -m model=glam_64 dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA
