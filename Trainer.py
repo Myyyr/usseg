770f2200b1890aa9d468e250fb92fffed8432824
Myr <myr@pop-os.localdomain>
2022-07-28 11:26:28 +0200

    cotr 64

--- Trainer.py
+++ Trainer.py
@@ -181,15 +181,15 @@ class Trainer():
                     ),
                 RandScaleIntensityd(keys="image", factors=0.1, prob=0.5),
                 RandShiftIntensityd(keys="image", offsets=0.1, prob=0.5),
-                # RandSpatialCropd(keys=["image", "label"],
-                # 	roi_size=self.crop_size,
-                # 	random_size=False),
-                RandCropByLabelClassesd(keys=["image", "label"],
-                						label_key="label",
-                						spatial_size=self.crop_size,
-                						num_classes=cfg.dataset.classes+1,
-                						num_samples=1
-                						)
+                RandSpatialCropd(keys=["image", "label"],
+                	roi_size=self.crop_size,
+                	random_size=False)
+                # RandCropByLabelClassesd(keys=["image", "label"],
+                # 						label_key="label",
+                # 						spatial_size=self.crop_size,
+                # 						num_classes=cfg.dataset.classes+1,
+                # 						num_samples=1
+                # 						)
                 # ToTensord(keys=["image", "label"]),
             ]
         )
