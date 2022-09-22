b672190cf719b40c886f2243a4e7428b48a77e37
Myr <myr@pop-os.localdomain>
2022-09-22 17:12:11 +0200

    usseg

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/cv5_nnunet_256.out # output file name
-#SBATCH --error=logs/cv5_nnunet_256.err  # error file name
+#SBATCH --output=logs/cv2_glam_256_ok.out # output file name
+#SBATCH --error=logs/cv2_glam_256_ok.err  # error file name
 
 ####################################################
 # b2$ sbatch job1.sh 							   #
@@ -73,7 +73,7 @@ conda activate usenv
 # [ cv4_nnunet_256 ]
 # srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv4
 # [ cv5_nnunet_256 ]
-srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv5
+# srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv5
 
 
 #### multi split
@@ -118,6 +118,25 @@ srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu da
 # srun python main.py -m model=glam_64 dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv4
 # [ cv5_glam_256 ]
 # srun python main.py -m model=glam_64 dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv5
+
+
+### GLAM_OK
+# [ cv2_glam_256_ok ]
+srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv2
+# --> CONTINUE  [ cv1_glam_256_continue_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv1 training.checkpoint.load=True
+# [ cv1_glam_256_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv1 
+# --> CONTINUE  [ cv1_glam_256_continue_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv1 training.checkpoint.load=True
+# [ cv3_glam_256_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv3
+# [ cv4_glam_256_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv4
+# [ cv5_glam_256_ok ]
+# srun python main.py -m model=glam_64_ok dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv5
+
+
 
 
 #### multi split
