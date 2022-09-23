a7e2d5d2d0198e2c10c0454a66d50d11754cfdef
Myr <myr@pop-os.localdomain>
2022-09-23 16:25:44 +0200

    usseg

--- launcher.sh
+++ launcher.sh
@@ -3,8 +3,8 @@
 #SBATCH --gpus=1
 #SBATCH --partition=long
 #SBATCH --time=100:00:00 
-#SBATCH --output=logs/cv1_nnunet_256.out # output file name
-#SBATCH --error=logs/cv1_nnunet_256.err  # error file name
+#SBATCH --output=logs/cv1_nnunet_256_continue.out # output file name
+#SBATCH --error=logs/cv1_nnunet_256_continue.err  # error file name
 
 ####################################################
 # b2$ sbatch job1.sh 							   #
@@ -70,9 +70,9 @@ conda activate usenv
 # srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv3 training.checkpoint.load=True
 
 # [ cv1_nnunet_256 ]
-srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv1
+# srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv1
 # --> CONTINUE [ cv1_nnunet_256_continue ]
-# srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv1 training.checkpoint.load=True
+srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA training.checkpoint.load=True dataset.cv=cv1 training.checkpoint.load=True
 
 # [ cv4_nnunet_256 ]
 # srun python main.py -m model=nnunet dataset=us256 training=crop64_128_128_nnu dataset.path.pth=/scratch/lthemyr/20220318_US_DATA training.pth=/scratch/lthemyr/20220318_US_DATA model.pth=/scratch/lthemyr/20220318_US_DATA dataset.cv=cv4
