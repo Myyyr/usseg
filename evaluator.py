047e3d3937bb875861129e2efd9989ab7d5e1cc4
Myr <myr@pop-os.localdomain>
2022-09-27 15:59:29 +0200

    usseg

--- evaluator.py
+++ evaluator.py
@@ -37,7 +37,7 @@ def main(pred_pth, gt_pth, out_pth):
 
 			# gt = zoom(gt, (0.3, 0.3, 0.3))
 			gt = spacer(gt)
-			pritn('debug', len(gt), type(gt))
+			print('debug', len(gt), type(gt))
 			size = gt.shape
 			print("a.2", gt.shape, pred.shape)
 
